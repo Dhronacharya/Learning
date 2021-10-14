80fdae18be968d0fee5e956665978a928bac1c5a
Harikrishnan <47082534+Dhronacharya@users.noreply.github.com>
2021-10-14 13:35:04 +0530

    Genesis File

---
+++ Test.dart
@@ -0,0 +1,29 @@
+import "dart:math";
+
+void main() {
+  // print(fibonoci(10));
+  print(Covered_Call_Ratio_Calculation(1000));
+
+}
+
+List fibonoci(int n) {
+  List<int> fibonociSeries = [];
+  for (int i = 0; i <= n; i++) {
+    if (fibonociSeries.length < 2) {
+      fibonociSeries.add(i);
+    } else {
+      fibonociSeries.add(fibonociSeries[i - 1] + fibonociSeries[i - 2]);
+    }
+  }
+  return fibonociSeries;
+}
+
+int Covered_Call_Ratio_Calculation(int num) {
+  String snum = num.toString();
+  if (snum.length <= 3 || num == 0) {
+    return int.parse(snum[0]);
+  } else if (snum.length > 3) {
+    return int.parse(snum.substring(0, snum.length - 2));
+  }
+  return -1;
+}
